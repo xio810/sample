2654333ffd3d55bad241cae5265cccb290f9e93c
xio810 <boi4806@naver.com>
2021-11-28 15:12:24 +0900

    기초 구성 & write 구현

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ssg start");
+
+        Scanner sc = new Scanner(System.in);
+
+        int lastArticleId = 0;
+
+        while (true) {
+            System.out.println("명령어 입력");
+            String command = sc.nextLine();
+
+            command = command.trim();
+
+            if (command.equals("exit")) {
+                break;
+            }
+
+            if (command.equals("write")) {
+                int id = lastArticleId + 1;
+                lastArticleId = id;
+                System.out.println("제목입력");
+                String title = sc.nextLine();
+                System.out.println("내용입력");
+                String body = sc.nextLine();
+
+                System.out.printf("%d번 글 생성\n", lastArticleId);
+            } else if (command.equals("list")) {
+                System.out.println("없음");
+            } else {
+                System.out.println(command + "은(는) 존재하지 않는 명령어 입니다.");
+            }
+        }
+        sc.close();
+
+        System.out.println("프로그램 종료");
     }
 }
