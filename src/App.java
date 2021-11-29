445aa4615231efb4c4f3adeeda0cd6b46c5c29c8
xio810 <boi4806@naver.com>
2021-11-29 19:28:30 +0900

    게시물 상세페이지 detail 구현

--- src/App.java
+++ src/App.java
@@ -50,6 +50,31 @@ public class App {
                     System.out.printf("%d    | %s\n", article.id, article.title);
                 }
 
+            } else if (command.equals("detail")) {
+                System.out.println("번호입력");
+                int id = sc.nextInt();
+
+                Article foundArticle = null;
+
+                for (int i = 0; i < articles.size(); i++) {
+                    Article article = articles.get(i);
+
+                    if (article.id == id) {
+                        foundArticle = article;
+                        break;
+                    }
+                }
+
+                if (foundArticle == null) {
+                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+                    continue;
+                }
+
+                System.out.printf("번호 : %d\n", foundArticle.id);
+                System.out.printf("날짜 : 2020-07-05 15:35:34\n");
+                System.out.printf("제목 : %s\n", foundArticle.title);
+                System.out.printf("내용 : %s\n", foundArticle.body);
+
             } else {
                 System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
             }
