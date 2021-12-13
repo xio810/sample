b4b4c0fe1ce4559a0d8bdd6110f4da1182fa042e
xio810 <boi4806@naver.com>
2021-12-13 19:55:39 +0900

    로그아웃 기능

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -31,13 +31,36 @@ public class MemberController extends Controller {
             case "login":
                 doLogin();
                 break;
+            case "logout":
+                doLogout();
+                break;
             default:
                 System.out.println("존재하지 않는 명령어 입니다.");
                 break;
         }
     }
 
+    private boolean isLogined() {
+        return loginedMember != null;
+    }
+
+    private void doLogout() {
+        if (isLogined() == false) {
+            System.out.println("로그인 상태가 아닙니다");
+            return;
+        }
+
+        loginedMember = null;
+        System.out.println("로그아웃 되었습니다.");
+    }
+
     private void doLogin() {
+
+        if (isLogined()) {
+            System.out.println("현재 로그인 상태 입니다");
+            return;
+        }
+
         System.out.printf("로그인 아이디 : ");
         String loginId = sc.nextLine();
         System.out.printf("로그인 비번 : ");
